b38b4da5580520c57957e306c06946fe7e7dbff8
IamHassanKhalid <ha3822655@gmail.com>
2021-08-06 10:59:15 +0500

    Initial Commit

---
+++ Fourth/Fourth/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fourth
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            String Name;
+
+            Console.WriteLine("Enter Your Name ");
+        
+           Console.WriteLine("Your Name is "+ Name);
+            
+            Console.ReadLine();
+
+
+           
+            
+
+
+
+        }
+    }
+}
