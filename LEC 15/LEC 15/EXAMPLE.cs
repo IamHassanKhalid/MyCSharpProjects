a3bc3220e349835201646e6386835310a5d1e674
IamHassanKhalid <ha3822655@gmail.com>
2021-08-06 11:09:56 +0500

    initial commit

---
+++ LEC 15/LEC 15/EXAMPLE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEC_15
+{
+    class EXAMPLE
+    {
+        int data;
+        public EXAMPLE()
+        {
+            data = 0;
+        }
+        public EXAMPLE(int x)
+        {
+            data = x;
+
+    }
+        public EXAMPLE(EXAMPLE obj)
+        {
+            data = obj.data;
+        }
+        public int display()
+        {
+            return data;
+        }
+    }
+}
---
+++ LEC 15/LEC 15/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEC_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EXAMPLE obj1 = new EXAMPLE(50);
+            label1.Text = "the obj1:" + obj1.display();
+            EXAMPLE obj2 = obj1;
+            label2.Text = "the obj2:" + obj2.display();
+
+        }
+    }
+}
